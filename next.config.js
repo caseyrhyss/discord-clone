3073bb008ce08d1fdef690f194b594ce593b3d0a
caseyrhyss <capaciocasey@gmail.com>
2023-09-11 14:23:46 +0800

    chore: downgrade

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.externals.push({
+            "utf-8-validate": "commonjs utf-8-validate",
+            butterutil: "commonjs butterutil"
+        });
+
+        return config;
+    },
     images: {
         domains: [
             "uploadthing.com",
